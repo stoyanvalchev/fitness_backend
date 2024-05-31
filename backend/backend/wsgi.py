9b98f51341f6d9a07b2eb95da8528eda6ae69fe0
Zarin Karanikolov <86721477+zarkobeats@users.noreply.github.com>
2024-05-31 13:55:25 +0300

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Path to your project directory
+path = '/home/zarinkaranikolov/fitness_backend/backend'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = get_wsgi_application()
